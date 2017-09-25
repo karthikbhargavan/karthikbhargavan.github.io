43b993f7b86b5fb763bddd44037ebc5ffc9ab330
karthikbhargavan <karthik.bhargavan@gmail.com>
2017-09-25 12:17:05 +0200

    current

--- thumbs/make-thumbs.sh
+++ thumbs/make-thumbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in `ls ../images/introduction`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/introduction/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 introduction/"$i"; done
-for i in `ls ../images/background`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/background/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 background/"$i"; done
+for i in `ls ../images/about`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/about/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 about/"$i"; done
+for i in `ls ../images/artist-bio`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/artist-bio/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 artist-bio/"$i"; done
 for i in `ls ../images/les-lumieres`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/les-lumieres/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 les-lumieres/"$i"; done
 for i in `ls ../images/the-plate-throwers`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/the-plate-throwers/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 the-plate-throwers/"$i"; done
 for i in `ls ../images/oceans-of-notions`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/oceans-of-notions/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 oceans-of-notions/"$i"; done
