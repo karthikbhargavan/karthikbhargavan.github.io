13f378efc6d2fac25152eee3105c5375d6a55604
karthikbhargavan <karthik.bhargavan@gmail.com>
2018-02-22 13:41:54 +0530

    current

--- make-dropbox-sync.sh
+++ make-dropbox-sync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avE --delete ~/Dropbox/SELINA-ROSY-MEUBLES/photos-for-website/ images/
+rsync -avE ~/Dropbox/SELINA-ROSY-MEUBLES/photos-for-website/ images/
--- thumbs/make-thumbs.sh
+++ thumbs/make-thumbs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 for i in `ls ../images/about`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/about/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 about/"thumb-$i"; done
+for i in `ls ../images/armour`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/armour/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 armour/"thumb-$i"; done
+for i in `ls ../images/fred`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/fred/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 fred/"thumb-$i"; done
 for i in `ls ../images/artist-bio`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/artist-bio/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 artist-bio/"thumb-$i"; done
 for i in `ls ../images/les-lumieres`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/les-lumieres/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 les-lumieres/"thumb-$i"; done
 for i in `ls ../images/the-plate-throwers`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/the-plate-throwers/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 the-plate-throwers/"thumb-$i"; done
@@ -7,3 +9,5 @@ for i in `ls ../images/oceans-of-notions`; do /usr/local/Cellar/imagemagick/7.0.
 for i in `ls ../images/parchemins`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/parchemins/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 parchemins/"thumb-$i"; done
 for i in `ls ../images/return-to-form`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/return-to-form/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 return-to-form/"thumb-$i"; done
 for i in `(cd ../images/surface-details && ls thumb-*)`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/surface-details/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 surface-details/"$i"; done
+for i in `(cd ../images/les-lumieres && ls thumb-*)`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/les-lumieres/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 les-lumieres/"$i"; done
+for i in `(cd ../images/armour && ls thumb-*)`; do /usr/local/Cellar/imagemagick/7.0.7-1/bin/convert ../images/armour/"$i" -thumbnail 100x80^ -gravity center -extent 100x80 armour/"$i"; done
